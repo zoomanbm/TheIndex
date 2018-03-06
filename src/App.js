c3a4f7d6b746f61eba1e2064b0498e5769790c83
Octowl <alsaff1987@gmail.com>
2018-03-06 14:14:57 +0300

    Data now comes from API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
-
-// Data
-import authors from './data';
+import axios from 'axios';
 
 // Components
 import Sidebar from './Sidebar';
 import AuthorsList from './AuthorsList';
 
+
+const instance = axios.create({
+  baseURL: "https://the-index-api.herokuapp.com"
+});
+
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      authors: []
+    }
+  }
+
+  componentDidMount() {
+    instance.get('/api/authors/')
+      .then(res => res.data)
+      .then(authors => this.setState({authors}))
+      .catch(err => console.error(err));
+  }
+
   render() {
     return (
       <div id="app" className="container-fluid">
@@ -16,7 +34,11 @@ class App extends Component {
             <Sidebar />
           </div>
           <div className="content col-10">
-            <AuthorsList authors={authors}/>
+            {
+              this.state.authors.length ?
+              <AuthorsList authors={this.state.authors}/> :
+              'loading...'
+            }
           </div>
         </div>
       </div>
