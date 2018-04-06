aec4fe41073760dfcea69bdd6001ab3892e47b2e
Octowl <alsaff1987@gmail.com>
2018-04-06 22:11:48 +0300

    Code styling

--- src/AuthorDetail.js
+++ src/AuthorDetail.js
@@ -3,11 +3,11 @@ import React from 'react';
 function AuthorDetail(props) {
 	return (
     <div className="author col-xs-10">
-		  <div>
-			<h3>I SHOULD BE AN AUTHOR NAME</h3>
-			<img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=IshouldBEanIMAGE&w=300&h=300" className="img-thumbnail"/>
+  		<div>
+  			<h3>I SHOULD BE AN AUTHOR NAME</h3>
+  			<img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=IshouldBEanIMAGE&w=300&h=300" className="img-thumbnail"/>
   		</div>
-  		<table className='table'>
+  		<table className='mt-3 table'>
   			<thead>
   				<tr>
   					<th>Name</th>
@@ -20,19 +20,19 @@ function AuthorDetail(props) {
   					<td>I SHOULD BE A BOOK NAME</td>
   					<td>I SHOULD BE A STRING OF THIS BOOK'S AUTHORS</td>
   					<td>
-              <button className="btn" style={{backgroundColor: "blue"}}/>
-            </td>
+  						<button className="btn" style={{backgroundColor: "blue"}}/>
+  					</td>
   				</tr>
-          <tr>
+  				<tr>
   					<td>I SHOULD BE ANOTHER BOOK NAME</td>
   					<td>I SHOULD BE A STRING OF THIS OHTER BOOK'S AUTHORS</td>
   					<td>
-              <button className="btn" style={{backgroundColor: "red"}}/>
-            </td>
+  						<button className="btn" style={{backgroundColor: "red"}}/>
+  					</td>
   				</tr>
   			</tbody>
   		</table>
-	  </div>
+    </div>
   );
 }
 
